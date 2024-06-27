bc10b1004e51e39b034eeb40b5165cbe9ebd3ab2
Ben Sicat <bensicat00@gmail.com>
2024-06-27 22:38:02 +0800

    learning about cmp and parsing and basics of rust

--- Personal/learning_rust/guessing_game/src/main.rs
+++ Personal/learning_rust/guessing_game/src/main.rs
@@ -1,15 +1,26 @@
-use std::io;
+use std:: io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Guessing game!");
 
     println!("Enter a number : ");
 
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
     let mut number = String::new();
     io::stdin()
         .read_line(&mut number)
         .expect("Failed to read line");
+    
+    let number: u32 = number.trim().parse().expect("Please do something idiot!");
+    
+    println!("You entered : {number}");
 
-    println!("You entered : {}", number);
-
+    match number.cmp(&secret_number){
+        Ordering:: Less => println!("Too small!"),
+        Ordering:: Greater => println!("Too big!"),
+        Ordering:: Equal => println!("You win!"),    
+    }
 
 }
