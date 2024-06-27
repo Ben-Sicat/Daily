74cd5d018b334e5d59fa43db5f5247ab9a272ecd
Ben Sicat <bensicat00@gmail.com>
2024-06-27 22:57:24 +0800

    added a looping function and breaking

--- Personal/learning_rust/guessing_game/src/main.rs
+++ Personal/learning_rust/guessing_game/src/main.rs
@@ -5,22 +5,28 @@ fn main() {
     println!("Guessing game!");
 
     println!("Enter a number : ");
-
+    
     let secret_number = rand::thread_rng().gen_range(1..=10);
-
-    let mut number = String::new();
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to read line");
-    
-    let number: u32 = number.trim().parse().expect("Please do something idiot!");
-    
-    println!("You entered : {number}");
-
-    match number.cmp(&secret_number){
-        Ordering:: Less => println!("Too small!"),
-        Ordering:: Greater => println!("Too big!"),
-        Ordering:: Equal => println!("You win!"),    
+    loop{
+        println!("Pleast enter a new number:");
+        
+            let mut number = String::new();
+            io::stdin()
+                .read_line(&mut number)
+                .expect("Failed to read line");
+            
+            let number: u32 = number.trim().parse().expect("Please do something idiot!");
+            println!("The secret number is: {secret_number}");
+            match number.cmp(&secret_number){
+                Ordering:: Less => println!("Too small!"),
+                Ordering:: Greater => println!("Too big!"),
+                Ordering:: Equal =>{
+                   println!("You win!");
+                   break; 
+                }     
+        }
+        
     }
+    
 
 }
